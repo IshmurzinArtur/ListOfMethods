0852149b6f66083fbbc829f896ea699ce610b54f
Artur Ishmurzin <hankbotrtt@gmail.com>
2019-07-11 05:32:32 +0400

    Добавьте файлы проекта.

---
+++ ListOfMethods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace ListOfMethods
+{
+    class Program
+    {
+        public static void GetInfo(string pathDir)
+        {
+            var dir = new DirectoryInfo(pathDir);
+            List<Assembly> assemblies = new List<Assembly>();
+            foreach (FileInfo fileInfo in dir.GetFiles("*.dll"))
+            {
+                assemblies.Add(Assembly.LoadFrom(fileInfo.FullName));
+            }
+            foreach (var assembly in assemblies)
+            {
+                try
+                {
+                    foreach (var type in assembly.GetTypes())
+                    {
+                        Console.WriteLine(type.Name);
+                        foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static))
+                        {
+                            if (method.IsFamily || method.IsPublic) //исключаем private
+                                Console.WriteLine("      " + method.Name);
+                        }
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            string pathDir = @"C:\Users\Artur\source\repos\ListOfMethods\ListOfMethods\assembly";
+            GetInfo(pathDir);
+        }
+    }
+}
